720ab92243dcd3d4fdc96db9468040bd9a4dac15
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2025-05-15 18:34:58 +0200

    Added type cmd

--- src/main.c
+++ src/main.c
@@ -2,31 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  while (1) {
-    // Flush after every printf
-    setbuf(stdout, NULL);
-    printf("$ ");
-    // Wait for user input
-    char input[100];
+#define CMD_LIST_SIZE 3
+
+char *cmd_list[] = {"echo", "exit", "type"};
+
+int is_equal(char *str1, char *str2) { return strcmp(str1, str2) == 0; }
+int is_builtin(char *command) {
+    for (int i = 0; i < CMD_LIST_SIZE; i++) {
+
+        if (is_equal(command, cmd_list[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void user_input(char *input) {
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+}
 
-    if (strcmp(input, "exit 0") == 0) {
-      break;
-    }
-
-    char *ptr = input;
-    char *cmd = strsep(&ptr, " ");
-
-    if (strcmp(cmd, "echo") == 0) {
-      if (ptr == NULL) {
+void execute_echo(char *arg) {
+    if (arg == NULL) {
         printf("\n");
-      }
-      printf("%s\n", ptr);
     } else {
-      printf("%s: command not found\n", cmd);
+        printf("%s\n", arg);
     }
-  }
-  return 0;
+}
+
+void execute_type(char *arg) {
+    char *first_arg = strsep(&arg, " ");
+    if (first_arg != NULL && is_builtin(first_arg) >= 0) {
+        printf("%s is a shell builtin\n", first_arg);
+    } else if (first_arg == NULL) {
+        printf("type needs an argument\n");
+    } else {
+        printf("%s: not found \n", first_arg);
+    }
+}
+
+void not_found(char *cmd) { printf("%s: command not found\n", cmd); }
+
+int main(int argc, char *argv[]) {
+    char input[100];
+
+    while (1) {
+        setbuf(stdout, NULL);
+        printf("$ ");
+
+        user_input(input);
+
+        char *args = input;
+        char *cmd = strsep(&args, " ");
+
+        if (is_equal(cmd, "exit")) {
+            break;
+        } else if (is_equal(cmd, "echo")) {
+            execute_echo(args);
+        } else if (is_equal(cmd, "type")) {
+            execute_type(args);
+        } else {
+            not_found(cmd);
+        }
+    }
+    return 0;
 }
